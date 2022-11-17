17236f9b0d02bb8e3b6aa84155da13406b96f9a5
Neil Bugeja <neilbugeja@pop-os.localdomain>
2022-11-17 16:07:58 +0100

    Initial Create

---
+++ installPrograms.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get update  && 
+sudo apt upgrade &&
+sudo apt install snapd &&
+snap install spotify &&
+sudo apt install git &&
+sudo dpkg -i code_1.73.1-1667967334_amd64.deb &&
+sudo apt-get update &&
+sudo apt upgrade
